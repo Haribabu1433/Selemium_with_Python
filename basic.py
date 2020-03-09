d3ecd2e422f7fcb18874b3d5c2922c5bc88036e9
pygit <antythin>
2020-03-09 17:41:55 +0530

    Initial commit

---
+++ basic.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\hpolicha\Downloads\chromedriver_win32\chromedriver.exe")
+#driver = webdriver.Firefox(executable_path=r"C:\Users\hpolicha\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe")
+
+driver.get("https://www.google.com")
+
+print(driver.title)
+#print(driver.current_url)
+
+driver.find_element_by_link_text("Images").click()
+
+print(driver.title)
+driver.back()
+time.sleep(5)
+print(driver.title)
+driver.forward()
+
+
+
+#driver.close()
+driver.quit()
